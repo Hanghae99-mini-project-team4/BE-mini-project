56856e4f0a3530a4b2fd74b05cfe6bb6aff18223
JongBeom Park <jb753858@gmail.com>
2022-12-19 12:32:51 +0900

    refactor: 댓글기능_3
    1. 댓글 기능 Mapper만 남기고 제거 완료.

--- src/main/java/com/mini/hanghae99miniproject/comment/controller/CommentController.java
+++ src/main/java/com/mini/hanghae99miniproject/comment/controller/CommentController.java
@@ -15,47 +15,24 @@ import org.springframework.web.bind.annotation.*;
 public class CommentController {
     private final CommentService commentService;
 
-    //댓글 작성
-    @PostMapping("/api/posts/comments/{postId}")
-    public DataResponse<CommentSaveResponseDto> createCommnet(@PathVariable Long postId, @AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody CommentSaveRequestDto requestDto) {
-        System.out.println("Controller userDetails.getMember().getId() = " + userDetails.getMember().getId());
-        System.out.println("Controller userDetails.getMember().getId() = " + userDetails.getMember().getUserid());
-        CommentSaveResponseDto commentSaveResponseDto = commentService.createComment(requestDto.toCommentDto(), userDetails.getMember(), postId);
-        return new DataResponse<>(ResponseMessage.CREATE_COMMENT_SUCCESS_MSG, commentSaveResponseDto);
-    }
-
     //댓글 작성 Version2
-    @PostMapping("/api/posts/{postId}/comments/test")
-    public DataResponse<ResponseComment> createCommnet2(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @RequestBody RequestComment requestComment) {
-        ResponseComment responseComment = commentService.createComment2(requestComment, userDetails.getMember(), postId);
+    @PostMapping("/api/posts/{postId}/comments")
+    public DataResponse<ResponseComment> createCommnet(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long postId, @RequestBody RequestComment requestComment) {
+        ResponseComment responseComment = commentService.createComment(requestComment, userDetails.getMember(), postId);
         return new DataResponse<>(ResponseMessage.CREATE_COMMENT_SUCCESS_MSG, responseComment);
     }
 
-    //댓글 수정
-    @PutMapping("/api/posts/comments/{commentId}")
-    public DataResponse<CommentUpdateResponseDto> updateComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId, @RequestBody CommentUpdateRequestDto requestDto){
-        CommentUpdateResponseDto commentUpdateResponseDto = commentService.updateComment(requestDto.toCommentDto(), userDetails.getMember(), commentId);
-        return new DataResponse<>(ResponseMessage.UPDATE_COMMENT_SUCCESS_MSG, commentUpdateResponseDto);
-    }
-
     //댓글 수정 Version2
-    @PutMapping("/api/posts/comments/{commentId}/test")
-    public DataResponse<ResponseComment> updateComment2(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId, @RequestBody RequestComment requestComment){
-        ResponseComment responseComment = commentService.updateComment2(requestComment, userDetails.getMember(), commentId);
+    @PutMapping("/api/posts/comments/{commentId}")
+    public DataResponse<ResponseComment> updateComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId, @RequestBody RequestComment requestComment){
+        ResponseComment responseComment = commentService.updateComment(requestComment, userDetails.getMember(), commentId);
         return new DataResponse<>(ResponseMessage.UPDATE_COMMENT_SUCCESS_MSG, responseComment);
     }
 
-    //댓글 삭제
+    //댓글 삭제 Version2
     @DeleteMapping("/api/posts/comments/{commentId}")
     public Response deleteComment(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId) {
         commentService.deleteComment(userDetails.getMember(), commentId);
-        return new Response((ResponseMessage.DELETE_COMMENT_SUCCESS_MSG));
-    }
-
-    //댓글 삭제 Version2
-    @DeleteMapping("/api/posts/comments/{commentId}/test")
-    public Response deleteComment2(@AuthenticationPrincipal UserDetailsImpl userDetails, @PathVariable Long commentId) {
-        commentService.deleteComment2(userDetails.getMember(), commentId);
         return new Response((ResponseMessage.DELETE_COMMENT_SUCCESS_MSG));
     }
 
--- src/main/java/com/mini/hanghae99miniproject/comment/service/CommentService.java
+++ src/main/java/com/mini/hanghae99miniproject/comment/service/CommentService.java
@@ -20,65 +20,22 @@ public class CommentService {
     private final PostRepository postRepository;
     private final MemberRepository memberRepository;
     private final CommentMapper commentMapper;
-    // 댓글 생성
-    @Transactional
-    public CommentSaveResponseDto createComment(CommentSaveDto commentSaveDto, Member member, Long postId){
-        //댓글 내용
-        String content = commentSaveDto.getContent();
-        System.out.println("content = " + content);
-        System.out.println("postId = " + postId);
-        System.out.println("member = " + member.getId());
-        System.out.println("member = " + member.getNickname());
-        //게시글 존재 여부 확인
-        Post post = postRepository.findById(postId).orElseThrow(
-                ()->      new IllegalArgumentException(ExceptionMessage.NO_EXIST_POSTING_ERROR_MSG.getMsg())
-        );
-
-        //유저 정보 확인
-        Member memberinfo = memberRepository.findById(member.getId()).orElseThrow(
-                ()-> new IllegalArgumentException(ExceptionMessage.USER_NOT_FOUND_ERROR_MSG.getMsg())
-        );
-        //댓글 저장
-        Comment comment = new Comment(content, member, post);
-        commentRepository.save(comment);
-        return new CommentSaveResponseDto(comment);
-    }
 
     //댓글 생성 Version2
     @Transactional
-    public ResponseComment createComment2(RequestComment requestComment, Member member, Long postId){
+    public ResponseComment createComment(RequestComment requestComment, Member member, Long postId){
         //게시글 존재 여부 확인
         Post post = postRepository.findById(postId).orElseThrow(
                 ()-> new IllegalArgumentException(ExceptionMessage.NO_EXIST_POSTING_ERROR_MSG.getMsg())
         );
-
         Comment comment = commentMapper.toComment(member, post, requestComment);
         commentRepository.save(comment);
         return commentMapper.toResponse(comment);
     }
-    //댓글 수정
-    @Transactional
-    public CommentUpdateResponseDto updateComment(CommentUpdateDto commentUpdateDto, Member member, Long commentId){
-        //댓글 내용
-        String content = commentUpdateDto.getContent();
-        //댓글 존재 확인
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
-                () -> new IllegalArgumentException(ExceptionMessage.NO_EXIST_COMMENT_ERROR_MSG.getMsg())
-        );
-        //댓글을 작성한 사람과 유저의 정보가 일치하는지 확인.
-        if(!comment.getMember().getId().equals(member.getId())){
-            throw new IllegalArgumentException(ExceptionMessage.USER_NOT_MATCH_ERROR_MSG.getMsg());
-        }
-        //댓글 내용 수정
-        comment.update(content);
-        return new CommentUpdateResponseDto(comment);
-    }
-
     //댓글 수정 Version2
     @Transactional
-    public ResponseComment updateComment2(RequestComment requestComment, Member member, Long commentId){
+    public ResponseComment updateComment(RequestComment requestComment, Member member, Long commentId){
         String content = requestComment.getContent();
-        System.out.println("content = " + content);
         //게시글 존재 여부 확인
         Comment comment = commentRepository.findById(commentId).orElseThrow(
                 () -> new IllegalArgumentException(ExceptionMessage.NO_EXIST_COMMENT_ERROR_MSG.getMsg())
@@ -88,27 +45,12 @@ public class CommentService {
             throw new IllegalArgumentException(ExceptionMessage.USER_NOT_MATCH_ERROR_MSG.getMsg());
         }
         comment.update(content);
-        System.out.println("comment = " + comment.getContent());
         return commentMapper.toResponse(comment);
-    }
-
-    //댓글 삭제
-    @Transactional
-    public void deleteComment(Member member, Long commentId) {
-        //댓글 존재 확인
-        Comment comment = commentRepository.findById(commentId).orElseThrow(
-                () -> new IllegalArgumentException(ExceptionMessage.NO_EXIST_COMMENT_ERROR_MSG.getMsg())
-        );
-        //댓글을 작성한 사람과 유저의 정보가 일치하는지 확인.
-        if(!comment.getMember().getId().equals(member.getId())){
-            throw new IllegalArgumentException(ExceptionMessage.USER_NOT_MATCH_ERROR_MSG.getMsg());
-        }
-        commentRepository.deleteById(commentId);
     }
 
     //댓글 삭제 Version2
     @Transactional
-    public void deleteComment2(Member member, Long commentId) {
+    public void deleteComment(Member member, Long commentId) {
         //댓글 존재 확인
         Comment comment = commentRepository.findById(commentId).orElseThrow(
                 () -> new IllegalArgumentException(ExceptionMessage.NO_EXIST_COMMENT_ERROR_MSG.getMsg())
