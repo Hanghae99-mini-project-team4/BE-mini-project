4c84de343f5f5f4e3c2281bfa59a46bfd1006274
sungwon <aogn07@naver.com>
2022-12-20 16:00:22 +0900

    refactor: Post Entity 삭제 복구
    Post Entity 삭제 복구

---
+++ src/main/java/com/mini/hanghae99miniproject/post/entity/Post.java
@@ -0,0 +1,53 @@
+package com.mini.hanghae99miniproject.post.entity;
+
+import com.mini.hanghae99miniproject.comment.entity.Comment;
+import com.mini.hanghae99miniproject.common.entity.Timestamped;
+import com.mini.hanghae99miniproject.member.entity.Member;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Post extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @OneToMany(mappedBy = "post")
+    private List<Comment> commentList = new ArrayList<>();
+
+    @Builder
+    public Post(String title, String content, Member member) {
+        this.title = title;
+        this.content = content;
+        this.member = member;
+    }
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
